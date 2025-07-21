6b5d09842e4e8b07d889148e13c7b3787160962e
Callum Rowston <109775779+CallumRowston@users.noreply.github.com>
2025-07-21 22:34:45 +1000

    Added basic auth; register, login, refresh

---
+++ backend/quiz/views_auth.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny
+from rest_framework import serializers
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
